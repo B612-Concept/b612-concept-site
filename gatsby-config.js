869f2394dfd5264ca2a21fb88d2ce4f8cf198e1e
gvjacob <gvjacob@outlook.com>
2020-10-29 20:00:53 -0400

    update project metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,22 @@
+function fromStatic(nodes) {
+  return `${__dirname}/static/${nodes.join('/')}`
+}
+
+const siteName = 'B612'
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: siteName,
+    description: `B612 is the only wellness experience service that provides a habitual way to disconnect and seclude in a private space for the introspective, achievers in Seattle WA who seek a mental getaway from the urban lifestyle in an era of digital exhaustion and hustle culture.`,
+    author: `Scout <neuscout411@gmail.com>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `assets`,
+        path: fromStatic('assets'),
       },
     },
     `gatsby-transformer-sharp`,
@@ -18,17 +24,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: siteName,
+        short_name: siteName,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: fromStatic('assets', 'gatsby-icon.png'),
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
