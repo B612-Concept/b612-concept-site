c5607a22bc9bf16702b7c7d4281a082239a10e47
gvjacob <gvjacob@outlook.com>
2020-11-10 21:38:19 -0500

    emoji flags for form countries

--- src/components/newsletter-form.js
+++ src/components/newsletter-form.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import flags, { data, US } from 'emoji-flags';
+
+import { franklinGothic, inputMono } from '@src/styles';
+
+const border = '1px solid black';
+
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: row;
+`;
+
+const CountriesWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  border: ${border};
+  border-left: 0;
+  font-size: 24px;
+
+  select {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const InputWrapper = styled.input`
+  padding: 20px;
+  font-family: ${inputMono};
+  border: ${border};
+`;
+
+const ButtonWrapper = styled.button`
+  padding: 20px;
+  font-family: ${inputMono};
+  border: ${border};
+  background-color: black;
+  color: white;
+`;
 
 const NewsletterForm = ({ className }) => {
+  const [email, setEmail] = useState('');
+  const [country, setCountry] = useState(US);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, country);
+  };
+
   return (
-    <form className={className}>
-      <input placeholder="Email address" />
-      <button type="submit">Subscribe</button>
-    </form>
+    <FormWrapper className={className} onSubmit={onSubmit}>
+      <InputWrapper
+        placeholder="Email address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <CountriesWrapper>
+        <div>{country.emoji}</div>
+        <select onChange={(e) => setCountry(flags[e.target.value])}>
+          {data.map(({ emoji, name, code }) => (
+            <option key={code} value={code}>
+              {name}
+            </option>
+          ))}
+        </select>
+      </CountriesWrapper>
+      <ButtonWrapper type="submit">Subscribe</ButtonWrapper>
+    </FormWrapper>
   );
 };
 
