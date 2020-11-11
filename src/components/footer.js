296e407700c253514c672c2ffe52c46c0c7f54c4
gvjacob <gvjacob@outlook.com>
2020-11-10 22:29:48 -0500

    style links on fotoer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
 
 import Link from '@src/components/link';
 import NewsletterForm from '@src/components/newsletter-form';
+import { inputMono } from '@src/styles';
+
+const LinksWrapper = styled.div`
+  padding: 20px;
+  font-family: ${inputMono};
+`;
+
+const UnderlineWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+const SocialLinksWrapper = styled.div`
+  text-align: right;
+`;
+
+const ScoutWrapper = styled.p`
+  text-align: center;
+`;
+
+const Rule = styled.hr`
+  margin: 20px 0;
+  opacity: 30%;
+`;
 
 const Footer = ({ className }) => {
   const {
@@ -40,17 +65,23 @@ const Footer = ({ className }) => {
       <h3>{headline}</h3>
       <p dangerouslySetInnerHTML={{ __html: newsletter_description }} />
       <NewsletterForm />
-      <p>{copyright}</p>
-      <div>
-        {navigation_links.map(({ label, url }) => (
-          <Link url={url}>{label}</Link>
-        ))}
-      </div>
-      <div>
-        {social_links.map(({ label, url }) => (
-          <Link url={url}>{label}</Link>
-        ))}
-      </div>
+      <LinksWrapper>
+        <div>
+          {navigation_links.map(({ label, url }) => (
+            <Link url={url}>{label}</Link>
+          ))}
+        </div>
+        <Rule />
+        <UnderlineWrapper>
+          <p>{copyright}</p>
+          <ScoutWrapper>Made with ❤️ by Scout</ScoutWrapper>
+          <SocialLinksWrapper>
+            {social_links.map(({ label, url }) => (
+              <Link url={url}>{label}</Link>
+            ))}
+          </SocialLinksWrapper>
+        </UnderlineWrapper>
+      </LinksWrapper>
     </footer>
   );
 };
