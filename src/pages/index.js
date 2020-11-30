ed10cf180f3f9cd214f3af9afda13e89af36a009
gvjacob <gvjacob@outlook.com>
2020-11-29 19:53:14 -0500

    render text content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,37 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
 
-const IndexPage = () => {
-  return 'hello';
+import { H1 } from '@src/components/fonts';
+
+const StyledH1 = styled(H1)`
+  font-weight: 300;
+`;
+
+const IndexPage = ({ data }) => {
+  const {
+    markdownRemark: { frontmatter },
+  } = data;
+
+  const { introduction, disclaimer, cta_text } = frontmatter;
+
+  return (
+    <section>
+      <StyledH1 dangerouslySetInnerHTML={{ __html: introduction }} />
+      <p dangerouslySetInnerHTML={{ __html: disclaimer }} />
+    </section>
+  );
 };
 
 export default IndexPage;
+
+export const query = graphql`
+  query LandingPageQuery {
+    markdownRemark(fields: { sourceName: { eq: "landing" } }) {
+      frontmatter {
+        introduction
+        disclaimer
+        cta_text
+      }
+    }
+  }
+`;
