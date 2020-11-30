344e1fda5c74642015d3ff15bfb4ea352fe2728b
gvjacob <gvjacob@outlook.com>
2020-11-29 20:40:16 -0500

    position introduction and disclaimer

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,51 @@ import styled from 'styled-components';
 
 import { H1 } from '@src/components/fonts';
 
-const StyledH1 = styled(H1)`
-  font-weight: 300;
+const IndexPageWrapper = styled.section`
+  position: relative;
+  height: 100vh;
 `;
+
+const Introduction = styled(H1)`
+  font-weight: 300;
+  position: absolute;
+
+  text-align: center;
+  margin: 0;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Disclaimer = styled.p`
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+
+  text-align: center;
+`;
+
+const Planet = styled.span`
+  background: rgba(255, 255, 255, 0.9);
+  box-shadow: 0px 0px 250px rgba(243, 156, 76, 0.5);
+  border-radius: 100%;
+
+  width: 20%;
+  padding-top: 20%;
+
+  position: absolute;
+  z-index: -1;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const CTA = ({ text }) => {
+  return <button></button>;
+};
 
 const IndexPage = ({ data }) => {
   const {
@@ -16,10 +58,11 @@ const IndexPage = ({ data }) => {
   const { introduction, disclaimer, cta_text } = frontmatter;
 
   return (
-    <section>
-      <StyledH1 dangerouslySetInnerHTML={{ __html: introduction }} />
-      <p dangerouslySetInnerHTML={{ __html: disclaimer }} />
-    </section>
+    <IndexPageWrapper>
+      <Planet />
+      <Introduction dangerouslySetInnerHTML={{ __html: introduction }} />
+      <Disclaimer dangerouslySetInnerHTML={{ __html: disclaimer }} />
+    </IndexPageWrapper>
   );
 };
 
