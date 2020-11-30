2568b431ecb837432a8b5ce3a5c2cb27c6a20166
gvjacob <gvjacob@outlook.com>
2020-11-29 19:27:41 -0500

    Move scenes to explore.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,9 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
-import Carousel from '@src/components/carousel';
-import Question from '@src/components/question';
-import SensesScene from '@src/components/sensesScene';
 
-const IndexPageWrapper = styled.div`
-  height: 100vh;
-`;
-
-const IndexPage = ({ data }) => {
-  const scenesData = data.markdownRemark.frontmatter;
-
-  return (
-    <IndexPageWrapper>
-      <Carousel data={scenesData} />
-    </IndexPageWrapper>
-  );
+const IndexPage = () => {
+  return 'hello';
 };
 
 export default IndexPage;
-
-export const query = graphql`
-  query IndexPageQuery {
-    markdownRemark(fields: { sourceName: { eq: "scenes" } }) {
-      frontmatter {
-        question
-        calm_scene {
-          body
-          title
-        }
-        getaway_scene {
-          body
-          title
-        }
-        imagine_scene {
-          body
-          title
-        }
-        journey_scene {
-          body
-          title
-        }
-        reflection_scene {
-          body
-          title
-        }
-        safe_scene {
-          body
-          title
-        }
-        senses_scene {
-          body
-          title
-        }
-      }
-    }
-  }
-`;
