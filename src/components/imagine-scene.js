80e654d6fd31c455d64545250b82342fcf69adb5
Jean Zhang <jean.nmn.zhang@gmail.com>
2020-12-08 12:24:50 -0500

    Images added to imagine scene

--- src/components/imagine-scene.js
+++ src/components/imagine-scene.js
@@ -3,70 +3,130 @@ import Box from './box';
 import styled from 'styled-components';
 import { max } from '@src/responsive';
 
-const IMG_PX_GAP = 75;
-const FIRST_IMG_LEFT_PX = -30;
-const HIGHEST_TOP_PX = -100;
+const getLeftPx = (index) =>
+  `calc(var(--first-img-left) + var(--img-gap) * ${index});`;
 
 const ImagineSceneWrapper = styled.div`
   position: relative;
+  --img-gap: 70px;
+  --first-img-left: -10px;
+  --highest-top: -120px;
+
+  @media ${max.tablet} {
+    --img-gap: 12%;
+    --highest-top: -70px;
+  }
+
+  @media ${max.tabletSm} {
+    --img-gap: 16%;
+    --first-img-left: 10px;
+    --highest-top: -90px;
+  }
 `;
 
 const ImagineImg = styled.img`
   position: absolute;
 `;
 
-const StarLeftImg = styled(ImagineImg)`
+// Star and Lightbulb Images
+const SmallImg = styled(ImagineImg)`
   width: 20px;
-  left: ${FIRST_IMG_LEFT_PX}px;
-  top: ${HIGHEST_TOP_PX + 40}px;
+
+  @media ${max.tablet} {
+    width: 15px;
+  }
 `;
 
-const LightLeftImg = styled(ImagineImg)`
-  width: 20px;
-  left: ${FIRST_IMG_LEFT_PX + IMG_PX_GAP}px;
-  top: ${HIGHEST_TOP_PX + 10}px;
+const StarLeftImg = styled(SmallImg)`
+  left: ${getLeftPx(0)};
+  top: calc(var(--highest-top) + 40px);
+`;
+
+const LightLeftImg = styled(SmallImg)`
+  left: ${getLeftPx(1)};
+  top: calc(var(--highest-top) + 20px);
+
+  @media ${max.tabletSm} {
+    display: none;
+  }
 `;
 
 const MoonImg = styled(ImagineImg)`
   width: 90px;
-  left: ${FIRST_IMG_LEFT_PX + IMG_PX_GAP * 2}px;
-  top: ${HIGHEST_TOP_PX + 60}px;
+  left: ${getLeftPx(2)};
+  top: calc(var(--highest-top) + 60px);
+
+  @media ${max.tablet} {
+    width: 80px;
+  }
+
+  @media ${max.tabletSm} {
+    left: ${getLeftPx(1)};
+  }
 `;
 
-const LightMiddleImg = styled(ImagineImg)`
-  width: 20px;
-  left: ${FIRST_IMG_LEFT_PX + IMG_PX_GAP * 3}px;
-  top: ${HIGHEST_TOP_PX}px;
+const LightMiddleImg = styled(SmallImg)`
+  left: ${getLeftPx(3)};
+  top: var(--highest-top);
+
+  @media ${max.tabletSm} {
+    left: ${getLeftPx(2)};
+  }
 `;
 
-const StarMiddleImg = styled(ImagineImg)`
-  width: 20px;
-  left: ${FIRST_IMG_LEFT_PX + IMG_PX_GAP * 4}px;
-  top: ${HIGHEST_TOP_PX + 40}px;
+const StarMiddleImg = styled(SmallImg)`
+  left: ${getLeftPx(4)};
+  top: calc(var(--highest-top) + 40px);
+
+  @media ${max.tabletSm} {
+    display: none;
+  }
 `;
 
-const LightRightImg = styled(ImagineImg)`
-  width: 20px;
-  left: ${FIRST_IMG_LEFT_PX + IMG_PX_GAP * 5}px;
-  top: ${HIGHEST_TOP_PX + 40}px;
+const LightRightImg = styled(SmallImg)`
+  left: ${getLeftPx(5)};
+  top: calc(var(--highest-top) + 40px);
+
+  @media ${max.tabletSm} {
+    display: none;
+  }
 `;
 
-const StarRightImg = styled(ImagineImg)`
-  width: 20px;
-  left: ${FIRST_IMG_LEFT_PX + IMG_PX_GAP * 6}px;
-  top: ${HIGHEST_TOP_PX + 60}px;
+const StarRightImg = styled(SmallImg)`
+  left: ${getLeftPx(6)};
+  top: calc(var(--highest-top) + 60px);
+
+  @media ${max.tabletSm} {
+    left: ${getLeftPx(3)};
+  }
 `;
 
 const PlanetLeftImg = styled(ImagineImg)`
   width: 50px;
-  left: ${FIRST_IMG_LEFT_PX + IMG_PX_GAP * 7}px;
-  top: ${HIGHEST_TOP_PX + 20}px;
+  left: ${getLeftPx(7)};
+  top: calc(var(--highest-top) + 20px);
+
+  @media ${max.tablet} {
+    width: 40px;
+  }
+
+  @media ${max.tabletSm} {
+    left: ${getLeftPx(4)};
+  }
 `;
 
 const PlanetRightImg = styled(ImagineImg)`
   width: 70px;
-  left: ${FIRST_IMG_LEFT_PX + IMG_PX_GAP * 8}px;
-  top: ${HIGHEST_TOP_PX}px;
+  left: ${getLeftPx(8)};
+  top: var(--highest-top);
+
+  @media ${max.tablet} {
+    width: 60px;
+  }
+
+  @media ${max.tabletSm} {
+    left: ${getLeftPx(5)};
+  }
 `;
 
 const ImagineScene = () => {
